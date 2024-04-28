b798e86d9f6dad817412728de1785f6f051aefd6
Danh <danhuynh1912@gmail.com>
2024-04-28 18:16:32 +0700

    Fix eslint

--- danhuynh-portfolio/src/App.tsx
+++ danhuynh-portfolio/src/App.tsx
@@ -1,18 +1,7 @@
-import { useState } from 'react';
-
 // Components
 import Section1 from './components/section1';
 
-// Assets
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
-// Styles
-// import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return <Section1 />;
 }
 
